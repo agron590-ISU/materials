41aa1e66de009b3934460c40fa87d16e4b12a7fc
Andee Kaplan <ajkaplan@iastate.edu>
2016-10-06 21:47:36 -0500

    add script

---
+++ week_07/programming/script.R
@@ -0,0 +1,52 @@
+## ----eval=FALSE----------------------------------------------------------
+## if(condition) {
+##   #code to run if condition is true
+## } else if(second condition) {
+##   #other code to run if second true
+## } else {
+##   #finally, code to run if conditions false
+## }
+
+## ------------------------------------------------------------------------
+x <- "hello"
+if (!is.numeric(x)) {
+    print("Numeric input is required")
+} else {
+  print(TRUE)
+}
+
+## ------------------------------------------------------------------------
+for(i in 1:2){
+  print(i)  
+}
+  
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+cols <- colnames(diamonds)
+for(i in cols) {
+  print(paste(i, ":", class(diamonds[, i])))
+}
+
+## ------------------------------------------------------------------------
+i <- 1
+while (i <= 5) {
+  print(i)
+  i <- i + 1
+}
+
+## ----eval=FALSE----------------------------------------------------------
+## foo <- function(params) {
+##   #code goes here
+##   return(output)
+## }
+
+## ------------------------------------------------------------------------
+foo <- function(n = 10) {
+  stopifnot(n > 0)
+  for(i in 1:n){
+    print("Hello, world \n")
+  }
+}
+foo(3)
+
