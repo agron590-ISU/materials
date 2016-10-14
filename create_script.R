20aacc102aed0c6e7054ff32cc7b907bdfd7e3fd
Andee Kaplan <ajkaplan@iastate.edu>
2016-10-13 19:25:49 -0500

    add slides for friday

--- create_script.R
+++ create_script.R
@@ -1,2 +1,4 @@
-file <- "slides.Rmd"
-knitr::purl(file, "script.R")
+file <- list.files(pattern="slides.*Rmd")
+category <- strsplit(strsplit(file, split = "_")[[1]][2], ".Rmd")[[1]]
+
+knitr::purl(file, paste0("script_", category, ".R"))
--- week_08/dplyr/script_dplyr.R
+++ week_08/dplyr/script_dplyr.R
@@ -15,9 +15,3 @@ group_by(baseball, id)
 summarise(baseball, mean(h))
 summarise(group_by(baseball, year), mean(h))
 
-## ---- message=FALSE------------------------------------------------------
-baseball %>%
-  group_by(id) %>%
-  summarise(ba = mean(h/ab)) %>% 
-  tail(2)
-
